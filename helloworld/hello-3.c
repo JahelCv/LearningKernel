bed8c5da72c836377811f4bbaaa47792c440c24d
Jahel Carmona Vila <jahelcarmonavila@gmail.com>
2022-09-28 20:33:23 +0200

    First commit

---
+++ helloworld/hello-3.c
@@ -0,0 +1,25 @@
+/*
+ * hello-3.c - Illustrating the __init, __initdata and __exit macros.
+ */
+ 
+#include <linux/init.h> /* Needed for the macros */ 
+#include <linux/kernel.h> /* Needed for pr_info() */ 
+#include <linux/module.h> /* Needed by all modules */ 
+
+static int hello3_data __initdata = 3;
+
+static int __init hello_3_init(void) 
+{
+    pr_info("Hello, world %d\n", hello3_data); 
+    return 0; 
+} 
+
+static void __exit hello_3_exit(void)
+{
+    pr_info("Goodbye, world 3\n");
+}
+
+module_init(hello_3_init);
+module_exit(hello_3_exit);
+
+MODULE_LICENSE("GPL");
